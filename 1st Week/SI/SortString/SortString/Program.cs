cb50292ec6594da2e104837b99dd879775066226
ujfalusizsolt1 <ujfalusi.zsolt1@gmail.com>
2018-08-22 06:06:45 +0200

    string solution

---
+++ 1st Week/SI/SortString/SortString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+            foreach (var item in words)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Array.Sort(words);
+            str = string.Join(" ", words);
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
